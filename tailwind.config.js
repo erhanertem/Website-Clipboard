a026f9904eaa20d22beced7bbb404a12ad1e0816
Erhan Ertem <eertem@gmail.com>
2023-05-27 17:54:05 +0300

    Complete supercharge section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.{html,js}'],
-	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
-		},
-		extend: {
-			colors: {
-				strongCyan: 'hsl(171,66%,44%)',
-				lightBlue: 'hsl(233,100%,69%)',
-				darkGrayishBlue: 'hsl(210,10%,33%)',
-				grayishBlue: 'hsl(201,11%,66%)',
-			},
-			fontFamily: {
-				sans: ['Bai Jamjuree', 'sans-serif'],
-			},
-			backgroundImage: {
-				'hero-pattern-desktop': "url('/images/bg-header-desktop.png')",
-				'hero-pattern-mobile': "url('/images/bg-header-mobile.png')",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        strongCyan: "hsl(171,66%,44%)",
+        lightBlue: "hsl(233,100%,69%)",
+        darkGrayishBlue: "hsl(210,10%,33%)",
+        grayishBlue: "hsl(201,11%,66%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-pattern-desktop": "url('/images/bg-header-desktop.png')",
+        "hero-pattern-mobile": "url('/images/bg-header-mobile.png')",
+      },
+    },
+  },
+  plugins: [],
 };
